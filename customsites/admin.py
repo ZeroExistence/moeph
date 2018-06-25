30eb3823418274eea88d8e080e77c1fd92c7ae83
ZeroExistence <tonydeguia009@gmail.com>
2018-06-25 00:15:25 +0000

    Removing customsites. Will be using siteinfo for site information.

--- customsites/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from customsites.models import CustomSite
-
-# Register your models here.
-
-admin.site.register(CustomSite)
--- customsites/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CustomsitesConfig(AppConfig):
-    name = 'customsites'
